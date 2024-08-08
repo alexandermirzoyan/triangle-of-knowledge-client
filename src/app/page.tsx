4287745746af0cf90d9c56fb608bb2c390e38140
Erik <erik.petri@yandex.ru>
2024-08-09 01:14:11 +0400

    TFK-17: added icons, images and favicons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ const Home = () => (
   <main className="flex min-h-screen flex-col items-center justify-between bg-grey-900">
     <Image
       className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-      src="/next.svg"
+      src="/triangle.svg"
       alt="Next.js Logo"
-      width={180}
-      height={37}
+      width={54}
+      height={48}
       priority
     />
   </main>
