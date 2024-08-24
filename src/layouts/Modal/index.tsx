185780ed52981e1e8bf03c52e279188a8a616a7c
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2024-08-24 18:46:38 +0400

    feat: Modal layout created, which is rendering on <body> tag with React Portal

---
+++ src/layouts/Modal/index.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { createPortal } from 'react-dom';
+import { IModalProps } from './types';
+
+export const Modal = ({ children, isVisible }: IModalProps) => {
+  if (!isVisible) {
+    return null;
+  }
+
+  return createPortal(
+    (
+      <div className='w-full h-full absolute top-0'>
+        {children}
+      </div>
+    ),
+    document.body,
+  );
+};
