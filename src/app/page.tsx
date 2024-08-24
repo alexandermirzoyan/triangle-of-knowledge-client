ca9e0389810b36e5a109253ec753dc1108ec2667
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2024-08-24 18:47:17 +0400

    feat: <Modal /> layout implemented at test page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,37 @@
 'use client';
 
-import { ThemeSwitcher } from '@/components/widgets/theme-switcher';
-import { ThemeContext } from '@/context/theme/context';
 import Image from 'next/image';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+
+import { ThemeContext } from '@/context/theme/context';
+import { ThemeSwitcher } from '@/components/widgets/theme-switcher';
+import { Modal } from '@/layouts/Modal';
 
 const Home = () => {
+  const [isModalVisible, setModalVisible] = useState(false);
   const { theme } = useContext(ThemeContext);
 
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between dark:bg-grey-700 ${theme.value}`}>
-      <ThemeSwitcher />
-      <div>
-        <p className='font-normal dark:text-white'>Welcome to TFK , Sign up</p>
-        <Image
-          className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-          src='/triangle.svg'
-          alt='Next.js Logo'
-          width={54}
-          height={48}
-          priority
-        />
-      </div>
-    </main>
+    <>
+      <main className={`flex min-h-screen flex-col items-center justify-between dark:bg-grey-700 ${theme.value}`}>
+        <ThemeSwitcher />
+        <button onClick={() => setModalVisible(!isModalVisible)}>{isModalVisible ? 'Hide Modal' : 'Show Modal'}</button>
+        <div>
+          <p className='font-normal dark:text-white'>Welcome to TFK , Sign up</p>
+          <Image
+            className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
+            src='/triangle.svg'
+            alt='Next.js Logo'
+            width={54}
+            height={48}
+            priority
+          />
+        </div>
+      </main>
+      <Modal isVisible={isModalVisible}>
+        <p>children</p>
+      </Modal>
+    </>
   );
 };
 
